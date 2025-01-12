f394e08895a60e7874860818381cb25976ee92e9
rahatalmas <rahat.almas5599@gmail.com>
2025-01-13 02:50:45 +0600

    dbr

--- controllers/result/resultController.js
+++ controllers/result/resultController.js
@@ -86,11 +86,11 @@ const addResult = async (req,res)=> {
         const [result] = await db.execute(resultQ.addResult,[exam_id,serial_number,correct_answers,incorrect_answers,grade]);
         //console.log([result]);
         res.status(201).json({"message":"New Result Added"});  
-        return;        
+               
     }catch(err){
         console.log(err);
         res.status(500).json({"message":"Internal Server Error"}); 
-        return;   
+          
     }
 }
 
